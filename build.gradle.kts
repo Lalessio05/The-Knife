d0ae115de7c1fd0eab2a92fbe7fa61a5352def20
Lalessio05 <92262963+Lalessio05@users.noreply.github.com>
2025-08-09 17:13:43 +0200

    Setup grafica tramite JavaFX

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.cupid404"
@@ -12,8 +13,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+javafx {
+    version = "24.0.2"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.cupid404.Main")
 }
