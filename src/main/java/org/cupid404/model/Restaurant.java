941db383ca77c01ae390fca71e471964cff338f3
Lalessio05 <92262963+Lalessio05@users.noreply.github.com>
2025-08-22 23:18:17 +0200

    Creati file json corretti per il ristorante, modificata la card del ristorante e la mainview in modo virtualmente definitivo. Implementato correttamente il model del ristorante e imbastita la creazione delle card (metodo MainViewController.initialize)

--- src/main/java/org/cupid404/model/Restaurant.java
+++ src/main/java/org/cupid404/model/Restaurant.java
@@ -2,37 +2,85 @@ package org.cupid404.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class Restaurant {
+    @JsonProperty("id")
+    public int id;
     @JsonProperty("Name")
     public String name;
     @JsonProperty("Address")
     public String address;
-    @JsonProperty("Location")
-    public String location;
-    @JsonProperty("Price")
-    public String price;
     @JsonProperty("Cuisine")
     public String cuisine;
     @JsonProperty("Longitude")
     public double longitude;
     @JsonProperty("Latitude")
     public double latitude;
-    @JsonProperty("PhoneNumber")
-    public String phoneNumber;
-    @JsonProperty("Url")
-    public String url;
-    @JsonProperty("WebsiteUrl")
-    public String websiteUrl;
-    @JsonProperty("Award")
-    public String award;
-    @JsonProperty("GreenStar")
-    public int greenStar;
-    @JsonProperty("FacilitiesAndServices")
-    public String facilitiesAndServices;
     @JsonProperty("Description")
     public String description;
+    @JsonProperty("City")
+    public String city;
+    @JsonProperty("State")
+    public String state;
+    @JsonProperty("OnlineBooking")
+    public boolean onlineBooking;
+    @JsonProperty("Delivery")
+    public boolean delivery;
+    @JsonProperty("AveragePrice")
+    public double averagePrice;
+    @JsonProperty("Reviews")
+    public List<Integer> reviews;
 
+    public int getId() {
+        return id;
+    }
     public String getName() {
         return name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public boolean isOnlineBooking() {
+        return onlineBooking;
+    }
+
+    public boolean isDelivery() {
+        return delivery;
+    }
+
+    public double getAveragePrice() {
+        return averagePrice;
+    }
+
+    public List<Integer> getReviews() {
+        return reviews;
+    }
+
 }
--- src/main/java/org/cupid404/ui/componentControllers/RestaurantCardController.java
+++ src/main/java/org/cupid404/ui/componentControllers/RestaurantCardController.java
@@ -1,20 +1,35 @@
 package org.cupid404.ui.componentControllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 
 public class RestaurantCardController {
 
-    @FXML
-    private ImageView imageView;
+    @FXML private VBox root;
+    @FXML private ImageView cardImage;
+    @FXML private Label titleLabel;
+    @FXML private Label addressLabel;
+    @FXML private Label priceLabel;
+    @FXML private Label ratingLabel;
 
-    @FXML
-    private Text restaurantName;
+    // "Costruttore" custom (in realtà metodo init) per valorizzare la card
+    public void setRestaurantData(String imageUrl,
+                         String title,
+                         String address,
+                         String price,
+                         String rating) {
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+            cardImage.setImage(new Image(imageUrl));
+        }
+        titleLabel.setText(title);
+        addressLabel.setText(address);
+        priceLabel.setText(price);
+        ratingLabel.setText(rating);
 
-    public void setRestaurantData(String name, String imageUrl) {
-        restaurantName.setText(name);
-        imageView.setImage(new Image(imageUrl));
     }
+
+
 }
